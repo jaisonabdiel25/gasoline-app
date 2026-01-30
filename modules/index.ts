ee45e6826890b124320b3c995b1c19c6471bce50
Jaison Palacio <jaisonpalaciodev@gmail.com>
2026-01-30 14:19:10 -0500

    feat: reestructurar formularios de pago y vehículo en módulos separados

--- modules/index.ts
+++ modules/index.ts
@@ -1,2 +1,4 @@
 export { TablePayment } from "./components/payments/TablePayment";
+export { FormPayment } from "./components/payments/FormPayment";
 export { AppSidebar } from "./components/sidebar/AppSidebar";
+export { FormVehicle } from "./components/vehicle/FormVehicle"
