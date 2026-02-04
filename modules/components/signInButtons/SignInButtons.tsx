33b84c60506a73ba92d541caf532f1503743bb55
Jaison Palacio <jaisonpalaciodev@gmail.com>
2026-02-03 20:14:42 -0500

    feat: reestructurar proyecto y agregar formulario de inicio de sesión con autenticación

---
+++ modules/components/signInButtons/SignInButtons.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { signIn } from "next-auth/react"
+
+type Props = {
+  providers: Awaited<ReturnType<typeof import("next-auth/react").getProviders>>
+}
+
+export default function SignInButtons({ providers }: Props) {
+  if (!providers) return null
+
+  return (
+    <div className="space-y-2">
+
+        
+      {Object.values(providers).map((provider) => {
+        if (provider.id === "credentials") return null
+
+        return (
+          <Button variant="outline" className="w-full"
+            key={provider.id}
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Continuar con {provider.name}
+          </Button>
+        )
+      })}
+    </div>
+  )
+}
